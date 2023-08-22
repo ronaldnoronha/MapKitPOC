8aee7d685768cfcae6104893084b1ac99748771a
Ronald Noronha <ronald.noronha@cba.com.au>
2023-08-22 13:24:08 +1000

    Initial Commit

---
+++ MapKitExample/MapKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapKitExampleApp.swift
+//  MapKitExample
+//
+//  Created by Noronha, Ronald on 22/8/2023.
+//
+
+import SwiftUI
+
+@main
+struct MapKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
