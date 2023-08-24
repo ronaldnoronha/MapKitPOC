806ecb711b6de7f8a3add670d28959f032b9b3a6
Ronald Noronha <ronald.noronha@cba.com.au>
2023-08-24 14:43:07 +1000

    Current location

--- MapKitExample/ContentView.swift
+++ MapKitExample/ContentView.swift
@@ -5,17 +5,68 @@
 //  Created by Noronha, Ronald on 22/8/2023.
 //
 
+import CoreLocation
+import MapKit
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+final class LocationManager: NSObject, ObservableObject {
+    private let locationManager = CLLocationManager()
+    
+    @Published var region = MKCoordinateRegion(
+        center: .init(latitude: -33.900_800, longitude: 151.190_300),
+        span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+    )
+    
+    override init() {
+        super.init()
+        
+        self.locationManager.delegate = self
+        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.setup()
+    }
+    
+    private func setup() {
+        switch locationManager.authorizationStatus {
+        case .authorizedWhenInUse:
+            locationManager.requestLocation()
+        case .notDetermined:
+            locationManager.startUpdatingLocation()
+            locationManager.requestWhenInUseAuthorization()
+        default:
+            break
         }
-        .padding()
+    }
+}
+
+extension LocationManager: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        guard .authorizedWhenInUse == manager.authorizationStatus else { return }
+        locationManager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Something went wrong: \(error)")
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        locations.last.map {
+            region = MKCoordinateRegion(
+                center: $0.coordinate,
+                span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            )
+        }
+    }
+}
+
+struct ContentView: View {
+    @StateObject var manager = LocationManager()
+    
+    var body: some View {
+        Map(
+            coordinateRegion: $manager.region,
+            showsUserLocation: true
+        )
+        .edgesIgnoringSafeArea(.all)
     }
 }
 
